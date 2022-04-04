7f253dd7ec7c328de64df7ef20ab607979d6ce20
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-04-04 00:32:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Reddit Place README Server
+
+Server application to simulate r/place but for README files
+
+## Requirements
+Go (https://go.dev/doc/install)
---
+++ server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		defaultPort := "8080"
+		port = defaultPort
+		fmt.Printf("Manually setting port to %s\n", defaultPort)
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.html")
+	router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	router.Run(":" + port)
+}
