98dc7c10f67ab64f49634c6886ee8ccfbd041377
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-04-04 00:53:14 -0400

    Add comments to code

--- server.go
+++ server.go
@@ -10,6 +10,7 @@ import (
 )
 
 func main() {
+	// get port form environment or default to 8080
 	port := os.Getenv("PORT")
 
 	if port == "" {
@@ -18,14 +19,33 @@ func main() {
 		fmt.Printf("Manually setting port to %s\n", defaultPort)
 	}
 
+	// create instance of gin router
 	router := gin.New()
+
+	// do not cache
+	router.Use(func() gin.HandlerFunc {
+		return func(c *gin.Context) {
+			c.Writer.Header().Set("Cache-Control", "no-cache, must-revalidate")
+			c.Writer.Header().Set("Pragma", "no-cache")
+			c.Writer.Header().Set("Expires", "Sat, 26 Jul 1997 05:00:00 GMT")
+		}
+	}(),
+	)
+
+	// use gin logger
 	router.Use(gin.Logger())
+
+	// load html from templates folder
 	router.LoadHTMLGlob("templates/*.html")
+
+	// load static content from static folder
 	router.Static("/static", "static")
 
+	// return index page for default route
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
+	// start the server
 	router.Run(":" + port)
 }
