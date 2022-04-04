c75b1e3b701ea505b0575b4c093c168fc2002fce
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-04-04 11:16:52 -0400

    Add comment

--- server.go
+++ server.go
@@ -14,6 +14,7 @@ import (
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
+// SetPixelRequestBody JSON format for set pixel request
 type SetPixelRequestBody struct {
 	X int   `json:"x"`
 	Y int   `json:"y"`
