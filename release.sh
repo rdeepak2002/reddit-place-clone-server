d20bdfbf0b20154550d42fcb5bfba6556e4d9d15
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-04-04 13:21:28 -0400

    added yarn file

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 echo "Running release script..."
-cd client && npm install --global rollup && yarn && yarn build
+cd client && npm install --global rollup && npm install --global rollup-plugin-svelte && yarn && yarn build
 echo "Done running release script!"
